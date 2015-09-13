f007d3cd3cd7f5b746416722ae53ea9bd7cd3f89
Susan Holcomb <thefurtheroff@gmail.com>
2015-09-12 20:58:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuantifiedSusan::Application.config.secret_token = '05818b924adefc83331b76ac2bc89a9847387bd54a13b0c5016c44652b5e5541460244f1d87fcd242e0792b1334cb700ec9bc625472aa63fe8e397dbf7e56d0d'
