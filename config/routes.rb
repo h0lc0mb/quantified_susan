79ed6bfd6e401f4a66aab709cf516cf72702e2c4
Susan Holcomb <thefurtheroff@gmail.com>
2015-09-13 20:13:42 -0700

    adding user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 QuantifiedSusan::Application.routes.draw do
+  resources :users
+
   root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
