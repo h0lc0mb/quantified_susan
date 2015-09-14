fb12dbd25ebe452278585334b09610af1bfb9a53
Susan Holcomb <thefurtheroff@gmail.com>
2015-09-13 22:17:08 -0700

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
